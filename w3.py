ebcbf33b554bc776f7a6dbbbafb95d2f4a12cefa
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 17:45:02 +0500

    Some info about lists

--- w3.py
+++ w3.py
@@ -1,17 +1,66 @@
-Initial commit:
-bool("abc")
-bool(123)
-bool(["apple", "cherry", "banana"])
-#These values are True, almost any number is True
+#Lists 
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+#Negative indexing
 
-bool(False)
-bool(None)
-bool(0)
-bool("")
-bool(())
-bool([])
-bool({})
-#But those are False values
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
 
-x = 200
-print(isinstance(x, int)) #Bult-in function that check whether the variable is of int data type
+#Ranging and splitting lists
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+#And vica versa
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+#Negative indexing 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+#Changing valuse inside a list
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+#Changing a range of items
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist) #inserting a valuse at a specified index
+#Append items
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+#Extend a value 
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist) 
+#Extending with typle type
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+#Removing the item
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+#Removing only first occurence
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+#Removing via an index
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+#Or via the del keyword
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist) 
+#clears the list, but it remains still(empty)
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
