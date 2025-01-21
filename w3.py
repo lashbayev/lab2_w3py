a0af96667f7250fca50a59aab85cc8718813cc29
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 22:52:01 +0500

    All methods and useful tips that left

--- w3.py
+++ w3.py
@@ -1,12 +1,84 @@
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist.copy()
-print(mylist)
+#tuples can have elements with the same value 
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
 
-thislist = ["apple", "banana", "cherry"]
-mylist = list(thislist)
-print(mylist)
+thistuple = ("apple",)
+print(type(thistuple))
 
-thislist = ["apple", "banana", "cherry"]
-mylist = thislist[:]
-print(mylist)
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
 
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+tuple1 = ("abc", 34, True, 40, "male") #different data types
+
+#Accessing tuple items
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+#
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+#
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+#Adding items
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+#Another way
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+#Removing
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+#Unpacking 
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
