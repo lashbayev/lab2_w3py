b36c5c411a1bae0e91a4b6dc7c0a3d5b56fe9080
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 18:44:23 +0500

    Three methods of copying

--- w3.py
+++ w3.py
@@ -1,38 +1,3 @@
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
-print(thislist)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort()
-print(thislist)
-#To sort in descending order:
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort(reverse = True)
-print(thislist)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(reverse = True)
-print(thislist)
-#Making sorting case-insensitive
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.sort()
-print(thislist)
-
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.sort(key = str.lower)
-print(thislist)
-#customizing sort function 
-def myfunc(n):
-  return abs(n - 50)
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort(key = myfunc)
-print(thislist)
-#Reverse method 
-thislist = ["banana", "Orange", "Kiwi", "cherry"]
-thislist.reverse()
-print(thislist)
-#Three methods of copying:
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
