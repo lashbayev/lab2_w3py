6ea6fe3aaed26e8c0a905fb7a071e88d05d3dca4
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 23:32:16 +0500

    For Loops

--- w3.py
+++ w3.py
@@ -1,25 +1,31 @@
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
 
-i = 1
-while i < 6:
-  print(i)
-  if i == 3:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
     break
-  i += 1
 
-i = 0
-while i < 6:
-  i += 1
-  if i == 3:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
     continue
-  print(i)
+  print(x)
 
-i = 1
-while i < 6:
-  print(i)
-  i += 1
-else:
-  print("i is no longer less than 6")
+for x in range(6):
+  print(x)
+
+for x in range(2, 30, 3):
+  print(x)
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y) #nested loops
+  
+for x in [0, 1, 2]:
+  pass
