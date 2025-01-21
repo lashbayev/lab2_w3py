1ac94bb88168a1fe81e46839a2df173b1a90cd65
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 18:41:06 +0500

    sorting list

--- w3.py
+++ w3.py
@@ -1,15 +1,47 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
 
-new_list = [x for x in fruits if x != 'apple']
-print(new_list)
-#List comprehension 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+#To sort in descending order:
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
 
-newlist = [x for x in fruits if "a" in x]
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+#Making sorting case-insensitive
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
 
-print(newlist) 
-#setting conditions in list comprehension
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x if x != "banana" else "orange" for x in fruits]
-print(newlist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+#customizing sort function 
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+#Reverse method 
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+Three methods of copying:
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
 
