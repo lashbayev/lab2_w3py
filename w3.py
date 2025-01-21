93a0f6812697eb92a6555b4cd181c45707dc24b1
Nurbek L. <nurbeklashbaevaz@gmail.com>
2025-01-21 23:25:54 +0500

    While Loops

--- w3.py
+++ w3.py
@@ -1,26 +1,25 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
 else:
-  print("a is greater than b")
-
-if a > b: print("a is greater than b")
-
-a = 33
-b = 200
-
-if b > a:
-  pass
-
-
-x = 41
-
-if x > 10:
-  print("Above ten,")
-  if x > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
+  print("i is no longer less than 6")
